2f950e0790b065b0a3380b00a2245612b208edc7
Arun Gupta <arun6445@gmail.com>
2021-02-21 15:34:51 +1100

    updated script

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-rsync --delete-before --verbose --archive /tmp/release/ /var/www/html/ >> /var/log/deploy.log
+sudo rsync --delete-before --verbose --archive /tmp/release/ /var/www/html/ >> /var/log/deploy.log
